c8b52f9b83d4e5d057f7ebb9fe639ee7c3ef5ac0
Alexander Durbin <ajd2@vt.edu>
2017-09-21 18:35:42 -0400

    Think i have the algorithm working now. but the differences cannot be made as small as i like, will ask kim tomorrow

--- hwk/hwk4/hw4.R
+++ hwk/hwk4/hw4.R
@@ -30,5 +30,37 @@ raw$Field <- sqrt(w) * raw$Field
 
 lmfit_new <- lm(Lab ~ Field, data = raw)
 
+if(all(coefficients(lmfit) - coefficients(lmfit_new)) < 1E-5) print('alex')
+
 # so assuming i am computing this correctly keep wrap this in a while loop 
 # after initial estimates old, new
+
+
+# new start ---------------------------------------------------------------
+
+raw <- read.table('data.txt', header = TRUE)
+y <- raw$Lab
+x <- raw$Field
+f1 <- lm(y ~ x)
+bo <- coefficients(f1)
+r2 <- residuals(f1)^2
+f2 <- glm(r2 ~ x, family = Gamma(link = 'log'), maxit = 100)
+w = rep(0, nrow(raw))
+w <- sapply(x, function(g) return(1 / exp(coefficients(f1)[1] + coefficients(f1)[2] * g)))
+y <- sqrt(w) * y
+x <- sqrt(w) * x
+bn <- coefficients(lm(y ~ x))
+
+while(all(abs(bn - bo) > 1E-5)){
+  
+  f1 <- lm(y ~ x)
+  bo <- coefficients(f1)
+  r2 <- residuals(f1)^2
+  f2 <- glm(r2 ~ x, family = Gamma(link = 'log'), maxit = 100)
+  w = rep(0, nrow(raw))
+  w <- sapply(x, function(g) return(1 / exp(coefficients(f1)[1] + coefficients(f1)[2] * g)))
+  y <- sqrt(w) * y
+  x <- sqrt(w) * x
+  bn <- coefficients(lm(y ~ x))
+  
+}

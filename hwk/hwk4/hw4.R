047641a60b1bb747aaa7fe1c5edd9d0da1943752
Alexander Durbin <ajd2@vt.edu>
2017-09-22 09:52:56 -0400

    Added regression line plots

--- hwk/hwk4/hw4.R
+++ hwk/hwk4/hw4.R
@@ -2,35 +2,36 @@
 # STAT5514 Homework 4
 # Dual Regression
 
-library(tidyverse)
-
-raw <- read.table('data.txt', header = TRUE)
-head(raw)
-
-lmfit <- lm(Lab ~ Field, data = raw)
-ob0 <- coefficients(lmfit)[1]
-ob1 <- coefficients(lmfit)[2]
-
-resid <- residuals(lmfit)
-resid2 <- resid^2
-
-glmfit <- glm(resid2 ~ raw$Field, family = Gamma(link = "log"))
-
-w = rep(0, nrow(raw))
-
-for(i in 1:nrow(raw)){
-  
-  w[i] = 1 / exp(ob0 + ob1 * raw$Field[i])
-  
-}
-
-
-raw$Lab <- sqrt(w) * raw$Lab
-raw$Field <- sqrt(w) * raw$Field
-
-lmfit_new <- lm(Lab ~ Field, data = raw)
-
-if(all(coefficients(lmfit) - coefficients(lmfit_new)) < 1E-5) print('alex')
+# library(tidyverse)
+# 
+# raw <- read.table('data.txt', header = TRUE)
+# head(raw)
+# 
+# lmfit <- lm(Lab ~ Field, data = raw)
+# ob0 <- coefficients(lmfit)[1]
+# ob1 <- coefficients(lmfit)[2]
+# 
+# resid <- residuals(lmfit)
+# resid2 <- resid^2
+# 
+# glmfit <- glm(resid2 ~ raw$Field, family = Gamma(link = "log"))
+# 
+# w = rep(0, nrow(raw))
+# 
+# for(i in 1:nrow(raw)){
+#   
+#   w[i] = 1 / exp(ob0 + ob1 * raw$Field[i])
+#   
+# }
+# 
+# w <- diag(w)
+# 
+# raw$Lab <- sqrt(w) * raw$Lab
+# raw$Field <- sqrt(w) * raw$Field
+# 
+# lmfit_new <- lm(Lab ~ Field, data = raw)
+# 
+# if(all(coefficients(lmfit) - coefficients(lmfit_new)) < 1E-5) print('alex')
 
 # so assuming i am computing this correctly keep wrap this in a while loop 
 # after initial estimates old, new
@@ -38,9 +39,17 @@ if(all(coefficients(lmfit) - coefficients(lmfit_new)) < 1E-5) print('alex')
 
 # new start ---------------------------------------------------------------
 
+library(tidyverse)
+library(MASS)
+
 raw <- read.table('data.txt', header = TRUE)
 y <- raw$Lab
 x <- raw$Field
+
+ggplot(data = raw) +
+  geom_jitter(mapping = aes(x = Field, y = Lab)) +
+  geom_smooth(mapping = aes(x = Field, y = Lab), method = 'lm', se = FALSE)
+
 f1 <- lm(y ~ x)
 bo <- coefficients(f1)
 r2 <- residuals(f1)^2
@@ -64,3 +73,13 @@ while(all(abs(bn - bo) > 1E-5)){
   bn <- coefficients(lm(y ~ x))
   
 }
+
+bo
+bn
+
+ggplot(data = raw) +
+  geom_jitter(mapping = aes(x = Field, y = Lab)) +
+  geom_smooth(mapping = aes(x = Field, y = Lab, color = 'lm'), method = 'lm', se = FALSE, fullrange = TRUE) +
+  geom_smooth(mapping = aes(x = Field, y = Lab, color = 'rlm'), method = 'rlm', se = FALSE, fullrange = TRUE) +
+  geom_abline(mapping = aes(color = 'dual', slope = bn[2], intercept = bn[1])) +
+  scale_color_manual(values = c('red', 'blue', 'orange'))

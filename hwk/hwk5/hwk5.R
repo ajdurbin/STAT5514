3e95cc4c6c3721cf360d4190f46aa513c47a6e24
Alexander Durbin <ajd2@vt.edu>
2017-10-03 10:25:25 -0400

    does not converge - singularity issue, kim says to fix alpha = 1 and ignore it as a parameter, and then modify d matrix and compute but chris gets singularity issue still

--- hwk/hwk5/hwk5.R
+++ hwk/hwk5/hwk5.R
@@ -4,18 +4,23 @@
 rm(list = ls())
 
 # formatting
-raw <- readxl::read_excel("Herbicide.xls")
-colnames(raw) <- NULL
-raw <- raw[2:8, 2:5]
-d1 <- raw[, c(2, 1)]
-colnames(d1) <- c('y', 'x')
-d2 <- raw[, c(4,3)]
-colnames(d2) <- c('y', 'x')
-d1$y <- as.numeric(d1$y)
-d2$y <- as.numeric(d2$y)
-d1$x <- as.numeric(d1$x)
-d2$x <- as.numeric(d2$x)
+# raw <- readxl::read_excel("Herbicide.xls")
+# raw <- read.csv("Herbicide.csv")
+# colnames(raw) <- NULL
+# raw <- raw[2:8, 2:5]
+# d1 <- raw[, c(2, 1)]
+# colnames(d1) <- c('y', 'x')
+# d2 <- raw[, c(4,3)]
+# colnames(d2) <- c('y', 'x')
+# d1$y <- as.numeric(d1$y)
+# d2$y <- as.numeric(d2$y)
+# d1$x <- as.numeric(d1$x)
+# d2$x <- as.numeric(d2$x)
 
+raw <- read.csv("Herbicide.csv")
+obs1 <- raw[1:7, ]
+x <- obs1$X
+y <- obs1$Y
 
 # functions for D matrix and residuals
 ass_fun <- function(data, old){
@@ -29,12 +34,12 @@ alpha_fun <- function(data, old){
 }
 
 beta_fun <- function(data, old){
-  val <- (old[1] * data ^old[3]) / (1 + old[2] * data ^old[3])^2
+  val <- (old[1] * data^old[3]) / (1 + old[2] * data^old[3])^2
   return(val)
 }
 
 gamma_fun <- function(data, old){
-  val <- (old[1] * old[2] * data * log(data)) / (1 + old[2] * data^old[3])^2
+  val <- (old[1] * old[2] * data^old[3] * log(data)) / (1 + old[2] * data^old[3])^2
   return(val)
 }
 
@@ -44,20 +49,23 @@ travel <- 1
 iter <- 1
 tol <- 1e-5
 maxiter <- 1000
-new <- c(0.01, 0.05, 0.1)
+new <- c(97, 0.1, -2.2)
 
 while(travel > tol && iter < maxiter){
   
   old <- new
-  r <- d1$y - ass_fun(d1$x, old)
-  sigma_sq <- r^2 / (nrow(d1) - 3)
-  v <- diag(sigma_sq)
-  d <- matrix(data = NA, nrow = nrow(d1), ncol = 3)
-  d[, 1] <- alpha_fun(d1$x, old)
-  d[, 2] <- beta_fun(d1$x, old)
-  d[, 3] <- gamma_fun(d1$x, old)
+  r <- y - ass_fun(x, old)
+  sigma_sq <- sum(r^2) / (nrow(obs1) - 3)
+  v <- diag(sigma_sq, nrow(obs1))
+  d <- matrix(data = NA, nrow = nrow(obs1), ncol = 3)
+  d[, 1] <- alpha_fun(x, old)
+  d[, 2] <- beta_fun(x, old)
+  d[, 3] <- gamma_fun(x, old)
   new <- old + solve(t(d) %*% solve(v) %*% d) %*% t(d) %*% solve(v) %*% r
   travel <- sum(abs(new - old))
   iter <- iter + 1
   
 }
+
+# nlslm to get initial values
+# just remove x from the model

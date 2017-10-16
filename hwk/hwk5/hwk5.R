37e2871fe2948d30fcc2e4727a12be70c74d0427
Alexander Durbin <ajd2@vt.edu>
2017-10-16 10:13:26 -0400

    adding robust nonlinear model code, need to double check with chris since sometimes im getting nlfit not found errors, also plotting is not good

--- hwk/hwk5/hwk5.R
+++ hwk/hwk5/hwk5.R
@@ -181,6 +181,10 @@ best <- nls2(y ~ (p1 * p2 * x^p3) / (1 + p2 * x^p3),
 coefficients(best)
 c(mean(b_coef[, 1]), mean(b_coef[, 2]), mean(b_coef[, 3]))
 
+# plot
+plot(x, y)
+lines(x, predict(best), lty = 2, col = "red", lwd = 3)
+
 # ci
 quantile(b_coef[, 1], probs = c(0.025, 0.975))
 quantile(b_coef[, 2], probs = c(0.025, 0.975))
@@ -211,3 +215,70 @@ quantile(b_coef[, 3], probs = c(0.025, 0.975))
 #                                  tol = 1e-1))
 # 
 # boot <- nlsBoot(fit, niter = 1000)
+
+
+# problem 1 ---------------------------------------------------------------
+
+
+rm(list = ls())
+options(stringsAsFactors = FALSE)
+library(minpack.lm)
+library(nls2)
+library(mgcv)
+library(robustbase)
+
+raw <- read.csv("EffRange.csv")
+raw <- raw[, 2:ncol(raw)]
+obs <- raw[, 1:2]
+colnames(obs) <- c('y', 'x')
+
+
+b_coef <- matrix(data = NA, ncol = 3, nrow = 10000)
+ss_res <- rep(0, 10000)
+
+for(i in 1:10000){
+  
+  boot <- sample(1:7, 7, replace = TRUE)
+  boot_sample <- obs[boot, ]
+  
+  try({
+    
+    nlfit <- nlrob(formula = y ~ a / (1 + b * exp(-k * x)), 
+                  start = list(a = 40, b = 400, k = 0.1),
+                  data = boot_sample,
+                  control = nls.control(maxiter = 1e3,
+                                    minFactor = .Machine$double.eps,
+                                    tol = 1e-5))
+  }, silent = TRUE)
+  
+  if(class(nlfit) == 'try-error'){
+    b_coef[i, ] <- NA
+  } else {
+    b_coef[i, ] <- coefficients(nlfit)
+    ss_res[i] <- sum(residuals(nlfit)^2)
+  }
+  
+  
+}
+
+dim(uniquecombs(b_coef))
+length(unique(ss_res))
+b_coef <- unique(b_coef)
+ss_res <- unique(ss_res)
+summary(ss_res)
+
+# compare best estimate from all 7 data points to mean of bootstrapped sample
+best <- nlrob(formula = y ~ a / (1 + b * exp(-k * x)),
+             start = list(a = 40, b = 400, k = 0.1),
+             data = obs)
+coefficients(best)
+c(mean(b_coef[, 1]), mean(b_coef[, 2]), mean(b_coef[, 3]))
+
+# plots
+plot(obs$x, obs$y)
+lines(obs$x, predict(best), lty = 2, col = "red", lwd = 3)
+
+# ci
+quantile(b_coef[, 1], probs = c(0.025, 0.975))
+quantile(b_coef[, 2], probs = c(0.025, 0.975))
+quantile(b_coef[, 3], probs = c(0.025, 0.975))
